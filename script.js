da0377863019d5690fbf1f40e41a34965bcf8f85
Blessing Emejulu <69702136+BlessingEmejulu@users.noreply.github.com>
2023-06-29 09:54:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+let checkBtn = document.getElementById("checkBtn");
+let clearBtn = document.getElementById("clearBtn");
+
+// Events
+checkBtn.addEventListener("click", checkEvenOrOdd);
+
+clearBtn.addEventListener("click", clearInput);
+
+// checking the integer
 function checkEvenOrOdd() {
-  var numberInput = document.getElementById("numberInput");
-  var result = document.getElementById("result");
+    let numberInput = document.getElementById("numberInput");
+    let result = document.getElementById("result");
 
-  // Get the input value and convert it to an integer
-  var number = parseInt(numberInput.value);
+    // Get the input value and convert it to an integer
+    let number = parseInt(numberInput.value);
 
-  if (number % 2 === 0) {
-    result.innerHTML = "The number is even.";
-  } else {
-    result.innerHTML = "The number is odd.";
-  }
+    if (number % 2 === 0) {
+        result.innerHTML = "The number is even.";
+    } else {
+        result.innerHTML = "The number is odd.";
+    }
 }
 
+// Clearing the input and output 
 function clearInput() {
-  var numberInput = document.getElementById("numberInput");
-  var result = document.getElementById("result");
+    let numberInput = document.getElementById("numberInput");
+    let result = document.getElementById("result");
 
-  numberInput.value = "";
-  result.innerHTML = "";
+    numberInput.value = "";
+    result.innerHTML = "";
 }
+
+  
